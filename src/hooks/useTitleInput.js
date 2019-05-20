4be7cf8bd7d84f50952c86499a52bc7f8b2574aa
avistheripper <bboyxns@mail.ru>
2019-05-20 20:58:46 +0300

    Custom hook added

---
+++ src/hooks/useTitleInput.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+
+export function useTitleInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+    useEffect(() => {
+      document.title = value;
+    });
+    return [value, setValue];
+  }
