3068da372bffff73146c1df1887087e446c7efcb
avistheripper <bboyxns@mail.ru>
2019-05-21 00:21:43 +0300

    useRef added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useRef }from 'react';
 import { Toggle } from './Toggle';
 import { ToggleState } from './ToggleUseState';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
   const [name, setName] = useTitleInput('');
+  const ref = useRef();
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper" ref={ref}>
       <h1>Level Up Dishes</h1>
       <Toggle />
       <ToggleState />
