4c4d71af671c4dd1f3e8876e009279c473cbc41d
avistheripper <bboyxns@mail.ru>
2019-05-24 11:36:11 +0300

    useDebugValue

--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
+
+
 
 export function useTitleInput(initialValue) {
     const [value, setValue] = useState(initialValue);
     useEffect(() => {
       document.title = value;
     });
+    useDebugValue(value);
     return [value, setValue];
   }
