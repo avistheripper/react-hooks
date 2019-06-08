b2e038beff92fed36ef6878eae69fb874fc4d10c
avistheripper <bboyxns@mail.ru>
2019-06-08 19:59:26 +0300

    Task resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,74 @@
-import React, { useMemo, useState, useEffect }from 'react';
-import { ToggleState } from './ToggleUseState';
-import { useTitleInput } from './hooks/useTitleInput';
-import { Counter } from './Counter';
+import React, { useEffect } from "react";
+import Iframe from "react-iframe";
 
-interface Address {
-  street: string;
-  city: string;
-  zipcode: string;
-}
+// no need actually
+interface PropsType {}
 
+export const App: React.SFC<PropsType> = () => {
+  const HOST_URL = "http://localhost:3001";
+  const defaultIframeHeight = 250;
 
-interface User {
-  name: string;
-  address: Address;
-  id: string;
-}
+  const getIframe = (selector: string): HTMLIFrameElement =>
+    document.getElementById(selector) as HTMLIFrameElement;
 
-export const App: React.FC<any> = () => {
-  const [name, setName] = useTitleInput('');
-  const title = 'Level Up Dishes'
-  const reverseWord = (str: string) => str
-      .split('')
-      .reverse()
-      .join('');
-  const initialState:  User[]= [];
-  const [users, setUsers] = useState(initialState);
-  const titleReversed = useMemo(() => reverseWord(title), [title]);
+  const getInput = (
+    targetDocument: Document,
+    selector: string
+  ): HTMLInputElement => targetDocument.getElementById(selector) as HTMLInputElement;
 
-  const getUsers = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-    setUsers(data);
-  }
-
-  useEffect(() => {
-    getUsers();
-  }, [])   // To run useEffect only with didMount second param is the empty array
-  return (
-      <div className="main-wrapper">
-      <h1>{ titleReversed }</h1>
-      <ToggleState />
-      <hr/>
-      <Counter />
-        <h3>Controlled state of SFC {name}</h3>
-        <form onSubmit={e => e.preventDefault()}>
-          <input
-            type="text"
-          />
-          <button>Submit</button>
-        </form>
-        {
-          users.map(user => (
-            <article className='dish-card dish-card--withImage'>
-              <h3>{ user.name }</h3>
-              <p>
-                  <div>
-                    <span>{ user.address.street }</span> |
-                    <span>{ user.address.city }</span> |
-                    <span>{ user.address.zipcode }</span>
-                  </div>
-              </p>
-            </article>
-          ))
+  const handleIframeTask = (e: EventListenerOrEventListenerObject | any): void => {
+    if (typeof e.data === "string" && e.origin === HOST_URL) {
+      let noteDisplayFrame = getIframe("notesList");
+      noteDisplayFrame.contentWindow.document.write(`
+        <input
+          type="text"
+          id="${e.data}"
+          value="${e.data}"
+        />
+        <button id="edit__${e.data}">Save</button>
+      `);
+      let targetFrameDOM = noteDisplayFrame.contentWindow.document;
+      let btnEdit = targetFrameDOM.getElementById(`edit__${e.data}`);
+      let input = getInput(targetFrameDOM, e.data);
+      let noteInputFrame = getIframe("noteInput");
+      if (targetFrameDOM.body.scrollHeight > defaultIframeHeight) {
+        noteDisplayFrame.height = `${targetFrameDOM.body.scrollHeight}px`;
+        noteInputFrame.height = `${targetFrameDOM.body.scrollHeight}px`;
+      }
+      btnEdit.addEventListener("click", () => {
+        if (input) {
+          handleNoteEdit(input.value);
         }
-        <br/>
-      </div>
+      });
+    }
+  };
+  const handleNoteEdit = (value: string) => {
+    let inputIframe = getIframe("noteInput");
+    let targetDOM = inputIframe.contentWindow.document;
+    let input = getInput(targetDOM, "noteName");
+    input.value = value;
+  };
+  useEffect(() => window.addEventListener("message", handleIframeTask), []);
+  return (
+    <div className="main-wrapper">
+      <Iframe
+        url=""
+        src="./input.html"
+        width="250px"
+        height={defaultIframeHeight + "px"}
+        id="noteInput"
+        display="inline"
+        className="iframe"
+      />
+      <Iframe
+        url=""
+        src="./display.html"
+        width="250px"
+        height={defaultIframeHeight + "px"}
+        id="notesList"
+        className="iframe"
+        display="inline"
+      />
+    </div>
   );
 };
-
-// const formSubmit = (value, setValue) => {
-//   console.log(`Email sent to ${value}!`);
-//   setValue('');
-// }
